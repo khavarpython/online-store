5e900afec60e414e073ed48c415f3bf12395be86
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-05 20:45:13 -0500

    Fixed the centering of the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,15 @@ import logo from "/logo.png";
 import { FaShoppingCart } from "react-icons/fa";
 function Header() {
   return (
-    <nav class="flex h-20 justify-between  items-center bg-gray-700 text-white ">
-      <div class="ml-5">
+    <nav class="flex w-full h-20 justify-between items-center bg-gray-700 text-white ">
+      <div class="ml-5 flex justify-between  ">
         <img src={logo} alt="Logo" class="w-20" />
       </div>
-      <div class="hidden sm:block">
-        <ul class="flex gap-1.5">
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
-          <li>Sale</li>
-        </ul>
+      <div class="hidden sm:flex ml-[16%] md:ml-[10%] lg:ml-[8%] gap-2">
+        <a>Men</a>
+        <a>Women</a>
+        <a>Kids</a>
+        <a>Jordan</a>
       </div>
       <div class="flex mr-5 gap-1">
         <input
