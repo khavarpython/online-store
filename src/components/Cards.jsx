22f2759faf3e65f0fc992cdddf305dd72582dcf0
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-06 01:08:46 -0500

    added cards to home pages

---
+++ src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import Card from "./Card";
+import card1 from "/card-1.jpg";
+import card2 from "/card-2.png";
+import card3 from "/card-3.jpg";
+function Cards() {
+  return (
+    <div class="flex justify-around mt-30 font-bold">
+      <Card img={card1} text="Cade Space 1s" />
+      <Card img={card2} text="Cade Space 2s" />
+      <Card img={card3} text="Cade Space 3s" />
+    </div>
+  );
+}
+export default Cards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Cards from "../components/Cards";
+
 function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Cards />
     </>
   );
 }
