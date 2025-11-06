88290cbc802564e97d72d984457c37f0ed2830dc
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-06 11:41:25 -0500

    added footer to home page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+function Footer() {
+  return (
+    <footer class="flex items-center h-30 justify-around bg-black text-gray-200 text-xs md:text-sm  ">
+      <ul>
+        <li>Shoes</li>
+        <li>Shoes</li>
+        <li>Shoes</li>
+      </ul>
+
+      <ul>
+        <li>Shoes</li>
+        <li>Shoes</li>
+        <li>Shoes</li>
+        <li>Shoes</li>
+        <li>Shoes</li>
+      </ul>
+      <ul>
+        <li>Shoes</li>
+        <li>Shoes</li>
+        <li>Shoes</li>
+        <li>Shoes</li>
+      </ul>
+    </footer>
+  );
+}
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Cards from "../components/Cards";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Header />
       <Hero />
       <Cards />
+      <Footer />
     </>
   );
 }
