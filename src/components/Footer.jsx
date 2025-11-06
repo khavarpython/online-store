59a8738a81dd1cfc4ba2c29f843271f5671fcb50
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-06 14:24:24 -0500

    Adjusted footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,25 @@
 function Footer() {
   return (
-    <footer class="flex items-center h-30 justify-around bg-black text-gray-200 text-xs md:text-sm  ">
-      <ul>
-        <li>Shoes</li>
-        <li>Shoes</li>
-        <li>Shoes</li>
-      </ul>
-
-      <ul>
-        <li>Shoes</li>
-        <li>Shoes</li>
-        <li>Shoes</li>
-        <li>Shoes</li>
-        <li>Shoes</li>
-      </ul>
-      <ul>
-        <li>Shoes</li>
-        <li>Shoes</li>
-        <li>Shoes</li>
-        <li>Shoes</li>
-      </ul>
+    <footer class="h-30  bg-black text-gray-200 text-xs md:text-sm  ">
+      <div class="flex text-center justify-around h-full ">
+        <ul class="self-center">
+          <li>Shoes</li>
+          <li>Clothing</li>
+          <li>Accessories</li>
+        </ul>
+        <ul class="self-center">
+          <li>New Arrivals</li>
+          <li>Best Sellers</li>
+          <li>Sale</li>
+          <li>Collections</li>
+        </ul>
+        <ul class="self-center">
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>FAQ</li>
+          <li>Returns</li>
+        </ul>
+      </div>
     </footer>
   );
 }
