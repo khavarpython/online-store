8730174bb4c59037e81c14775c3ca4fe3247e82f
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-07 15:11:58 -0500

    Added naviagtion and appropriate picture and headings to the category section

--- src/components/Home/Category.jsx
+++ src/components/Home/Category.jsx
@@ -1,14 +1,29 @@
 import CategoryCard from "./CategoryCard";
-
+import male from "../../assets/male.jpg";
+import woman from "../../assets/woman.webp";
+import kid from "../../assets/kid.jpg";
+import jordan from "../../assets/jordan.avif";
+import { useNavigate } from "react-router-dom";
 function Category() {
+  const navigate = useNavigate();
+  const handleNavigate = (input) => {
+    navigate(`/products`);
+  };
   return (
-    <div class="mx-8 my-8">
-      <h2 class="text-4xl font- my-1.5 font-serif font-black">Shop Now</h2>
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
-        <CategoryCard />
-        <CategoryCard />
-        <CategoryCard />
-        <CategoryCard />
+    <div class="mx-8 mb-12">
+      <h2
+        class="text-4xl font- mb-5 font-serif font-black "
+        onClick={() => {
+          handleNavigate();
+        }}
+      >
+        Shop Now
+      </h2>
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+        <CategoryCard img={male} text="Men" />
+        <CategoryCard img={kid} text="Kids" />
+        <CategoryCard img={jordan} text="Jordan" />{" "}
+        <CategoryCard img={woman} text="Women" />
       </div>
     </div>
   );
--- src/components/Home/CategoryCard.jsx
+++ src/components/Home/CategoryCard.jsx
@@ -1,10 +1,24 @@
-import male from "../../assets/male.avif";
-function CategoryCard() {
+import { useNavigate } from "react-router-dom";
+
+function CategoryCard(props) {
+  const navigate = useNavigate();
+  const handleNavigate = (input) => {
+    navigate(`/products/${input}`);
+  };
   return (
-    <div class="relative h-96  w-80 ">
-      <img class="absolute inset-0 max-h-100" src={male} alt="male model" />
-      <div class="absolute flex text-center top-0 left-0 right-0">
-        <h3 class="text-white font-extrabold text-4xl">Mens</h3>
+    <div
+      class="relative h-96 w-80 cursor-pointer"
+      onClick={() => {
+        handleNavigate(props.text);
+      }}
+    >
+      <img
+        class="absolute inset-0 object-cover max-h-96 max-w-80 hover:border-2"
+        src={props.img}
+        alt="model"
+      />
+      <div class="absolute bottom-0 left-0 right-0">
+        <h3 class="text-white font-extrabold text-4xl ml-2">{props.text}</h3>
       </div>
     </div>
   );
