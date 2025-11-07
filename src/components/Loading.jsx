29c5ad7dc3693cece231d0ff16d7a43cf776a762
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-07 15:11:11 -0500

    Added loading screen to the product list page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import spinner from "../assets/spinner.gif";
+function Loading() {
+  return (
+    <div class="fixed inset-0 bg-white z-9999 w-screen h-screen  ">
+      <img
+        class="w-screen h-screen mx-auto object-cover"
+        src={spinner}
+        alt="spinner"
+      />
+    </div>
+  );
+}
+export default Loading;
--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import Loading from "../Loading";
 
 function ProductList() {
   const [sneakers, setSneakers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  let param = useParams();
   useEffect(() => {
-    const url =
-      "https://the-sneaker-database.p.rapidapi.com/sneakers?limit=10&gender=MEN";
+    setLoading(true);
+    let url;
+    if (param.postId === undefined) {
+      url = "https://the-sneaker-database.p.rapidapi.com/sneakers?limit=10";
+    } else if (param.postId == "JORDAN") {
+      url =
+        "https://the-sneaker-database.p.rapidapi.com/sneakers?limit=10&brand=AIR%20JORDAN";
+    } else {
+      url = `https://the-sneaker-database.p.rapidapi.com/sneakers?limit=10&gender=${param.postId}`;
+    }
     const options = {
       method: "GET",
       headers: {
@@ -18,44 +30,48 @@ function ProductList() {
         const response = await fetch(url, options);
         const result = await response.json();
         setSneakers(result.results);
+        setLoading(false);
       } catch (error) {
         console.error(error);
       }
     }
     fetchSneaker();
-  }, []);
+  }, [param]);
 
-  useEffect(() => {
-    console.log(sneakers);
-  }, [sneakers]);
   return (
     <div>
-      <div class="ml-10 mt-5">
-        <Link to="/"> Back</Link>
-        <h1 class="text-4xl"> Men Shoes</h1>
-      </div>
-      <div class="grid grid-cols-2 sm:grid-cols-3 gap-y-5 max-w-6xl mx-auto my-5">
-        {sneakers.map((sneaker, index) => {
-          if (sneaker.image.original) {
-            return (
-              <div class="hover:border-2" key={sneaker.id}>
-                <img
-                  class="object-cover"
-                  src={sneaker.image.original}
-                  alt={sneaker.name}
-                />
-                <div class="ml-0.5 max-w-[95%]">
-                  <p>${sneaker.retailPrice}</p>
-                  <h4 class="capitalize">{sneaker.name}</h4>
-                  <p class="capitalize text-sm text-gray-500">
-                    {sneaker.brand}
-                  </p>
-                </div>
-              </div>
-            );
-          }
-        })}
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div class="ml-10 mt-5">
+            <Link to="/"> Back</Link>
+            <h1 class="text-4xl capitalize"> Shoes</h1>
+          </div>
+          <div class="grid grid-cols-2 sm:grid-cols-3 gap-y-5 max-w-6xl mx-auto my-5">
+            {sneakers.map((sneaker) => {
+              if (sneaker.image.original) {
+                return (
+                  <div class="hover:border-2" key={sneaker.id}>
+                    <img
+                      class="object-cover"
+                      src={sneaker.image.original}
+                      alt={sneaker.name}
+                    />
+                    <div class="ml-0.5 max-w-[95%]">
+                      <p>${sneaker.retailPrice}</p>
+                      <h4 class="capitalize">{sneaker.name}</h4>
+                      <p class="capitalize text-sm text-gray-500">
+                        {sneaker.brand}
+                      </p>
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
