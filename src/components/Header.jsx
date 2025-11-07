bcc30746fc699a815ab1647da54f13a2afee612d
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-07 15:10:21 -0500

    Added navigation to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,34 @@
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { FaShoppingCart } from "react-icons/fa";
+
 function Header() {
+  const navigate = useNavigate();
+  const handleNavigate = input => {
+    navigate(`/products/${input}`);
+  };
+
   return (
     <nav class="flex w-full h-20 justify-between items-center bg-black text-white ">
       <div class="ml-5 flex justify-between  ">
         <img src={logo} alt="Logo" class="w-20" />
       </div>
-      <div class="hidden sm:flex text-lg ml-[13%] md:ml-[10%] lg:ml-[8%] md:text-xl gap-2">
-        <a>Men</a>
-        <a>Women</a>
-        <a>Kids</a>
-        <a>Jordan</a>
+      <div class="hidden sm:flex text-lg ml-[13%] md:ml-[10%] lg:ml-[8%] md:text-xl gap-4">
+        <a class="cursor-pointer hover:underline hover:text-[1.5rem]" onClick={() => handleNavigate("MEN")}>
+          Men
+        </a>
+        <a class="cursor-pointer hover:underline hover:text-[1.5rem]" onClick={() => handleNavigate("WOMEN")}>
+          Women
+        </a>
+        <a class="cursor-pointer hover:underline hover:text-[1.5rem]" onClick={() => handleNavigate("KIDS")}>
+          Kids
+        </a>
+        <a class="cursor-pointer hover:underline hover:text-[1.5rem]" onClick={() => handleNavigate("JORDAN")}>
+          Jordan
+        </a>
       </div>
       <div class="flex mr-5 gap-2">
-        <input
-          class="w-40 h-7 bg-white text-gray-700 "
-          type="text"
-          placeholder=" Search"
-        />
+        <input class="w-40 h-7 bg-white text-gray-700 " type="text" placeholder=" Search" />
         <FaShoppingCart class="size-6" />
       </div>
     </nav>
