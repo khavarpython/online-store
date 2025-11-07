c8b1a502fb427e0726fd70a4ec47549e6691b7eb
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-06 22:12:31 -0500

    Added products page to site

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Product from "./pages/Product";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Product />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </>
   );
---
+++ src/pages/Products.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import ProductList from "../components/Products/ProductList";
+
+function Products() {
+  return (
+    <>
+      <Header />
+      <ProductList />
+      <Footer />
+    </>
+  );
+}
+export default Products;
