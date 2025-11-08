41cb64039e6c33b2726d97a6f013e695c2b594c0
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-07 20:24:26 -0500

    Added new route for product page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import Product from "./pages/Product";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products/" element={<Products />} />
         <Route path="/products/:postId" element={<Products />} />
+        <Route path="/product/:postId" element={<Product />} />
       </Routes>
     </>
   );
