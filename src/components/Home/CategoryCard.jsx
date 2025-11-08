bf77ff284728a71d8352bf502915604bcc0f9269
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-07 20:21:58 -0500

    Added navigation to the category cards

--- src/components/Home/CategoryCard.jsx
+++ src/components/Home/CategoryCard.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 function CategoryCard(props) {
   const navigate = useNavigate();
-  const handleNavigate = (input) => {
+  const handleNavigate = input => {
     navigate(`/products/${input}`);
   };
   return (
@@ -12,11 +12,7 @@ function CategoryCard(props) {
         handleNavigate(props.text);
       }}
     >
-      <img
-        class="absolute inset-0 object-cover max-h-96 max-w-80 hover:border-2"
-        src={props.img}
-        alt="model"
-      />
+      <img class="absolute inset-0 object-cover max-h-96 max-w-80 hover:border-2" src={props.img} alt="model" />
       <div class="absolute bottom-0 left-0 right-0">
         <h3 class="text-white font-extrabold text-4xl ml-2">{props.text}</h3>
       </div>
