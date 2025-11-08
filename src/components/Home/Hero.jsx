2bb5101cdc164053ae32a55eeb0c12102219cd19
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-07 20:22:50 -0500

    Added navigation to hero button

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,14 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import hero_video from "../../assets/hero.mp4";
 function Hero() {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/products");
+  };
   return (
     <div class="relative w-full h-screen">
-      <video
-        class="absolute inset-0 w-full h-full object-cover"
-        disablePictureInPicture
-        loop="loop"
-        autoplay="autoplay"
-        muted
-      >
+      <video class="absolute inset-0 w-full h-full object-cover" disablePictureInPicture loop="loop" autoplay="autoplay" muted>
         <source src={hero_video} type="video/mp4"></source>
         Your browser does not support the video tag.
       </video>
@@ -16,7 +15,12 @@ function Hero() {
       <div class="absolute bottom-0 left-0 right-0 pb-12 flex flex-col items-center text-white text-center ">
         <h2 class="text-7xl font-extrabold">Online Store</h2>
         <p class="text-sm">This website was designed by Khavar Facey</p>
-        <button class="bg-white text-gray-700 px-5 py-1 rounded-xl mt-2 cursor-pointer">
+        <button
+          class="bg-white text-gray-700 px-5 py-1 rounded-xl mt-2 cursor-pointer hover:bg-gray-300"
+          onClick={() => {
+            handleNavigate();
+          }}
+        >
           Shop
         </button>
       </div>
