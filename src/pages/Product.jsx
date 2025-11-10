cd86bace82563550c1f348f8dcd24bd6e1b1a8c7
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-10 12:01:16 -0500

    Added success text for cart

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useState, useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import { IoIosArrowBack } from "react-icons/io";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { shoeSizes } from "../components/Product/sizeData";
-import { IoIosArrowBack } from "react-icons/io";
-import { Link, useParams } from "react-router-dom";
 import Loading from "../components/Loading";
+import { shoeSizes } from "../components/Product/sizeData";
 
 function Product() {
   const [sneakers, setSneakers] = useState([]);
   const [colors, setColors] = useState([]);
   const [loading, setLoading] = useState(true);
   const [size, setSize] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [add, setAdd] = useState(false);
   const { cartItems, addToCart } = useContext(CartContext);
+  const navigate = useNavigate();
   let param = useParams();
 
   const decodeHTML = (html) => {
@@ -66,11 +69,14 @@ function Product() {
 
           <div class="flex my-15 mx-5 justify-center gap-5 ">
             <div class="max-w-lg h-full">
-              <Link to="/">
-                <button class="flex gap-0.5 items-center justify-center hover:bg-black hover:text-white pr-3 rounded-lg">
-                  <IoIosArrowBack /> Back
-                </button>
-              </Link>
+              <button
+                class="flex gap-0.5 items-center justify-center hover:bg-black hover:text-white px-3 py-2 rounded-lg"
+                onClick={() => {
+                  navigate(-1);
+                }}>
+                <IoIosArrowBack /> Back
+              </button>
+
               <img src={sneaker.image.original} alt="" class="mt-5" />
             </div>
 
@@ -98,9 +104,10 @@ function Product() {
                   {shoeSizes[sneaker.gender].map((size, index) => {
                     return (
                       <button
-                        class="border w-12 px-2 py-1 rounded-sm  hover:border focus-within:border-2 focus-within:border-green-600"
+                        class="border w-12 px-2 py-1 rounded-sm hover:border-2"
                         key={index}
                         onClick={() => {
+                          setShowError(false);
                           setSize(size.size);
                         }}>
                         {size.size}
@@ -108,23 +115,28 @@ function Product() {
                     );
                   })}
                 </fieldset>
+                <p class={showError ? "block text-red-600" : "hidden"}>Please select a size</p>
                 <button
-                  class=" my-5 bg-black text-white px-3 py-2 rounded-md hover:bg-gray-700"
+                  class="mt-5 mb-2 bg-black text-white px-3 py-2 rounded-md hover:bg-gray-700"
                   onClick={() => {
                     if (!size) {
-                      alert("Please Select a Size");
+                      setShowError(true);
                     } else {
+                      setAdd(true);
                       addToCart(sneaker, size);
                       setSize("");
                     }
                   }}>
                   Add to Cart
                 </button>
+                <p class={add ? "block text-lg font-bold text-green-500 mb-5" : "hidden"}>
+                  {" "}
+                  Added to Cart Successfully{" "}
+                </p>
               </div>
               <p class="text-lg/relaxed">{decodeHTML(sneaker.story)} </p>
             </div>
           </div>
-
           <Footer />
         </>
       )}
