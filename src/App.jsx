df125e9d4ab99386ecfdfe74b63f0cb7bf7311cf
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-10 00:08:11 -0500

    Created cart page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Product from "./pages/Product";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/products/" element={<Products />} />
         <Route path="/products/:postId" element={<Products />} />
         <Route path="/product/:postId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </>
   );
