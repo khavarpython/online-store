26ba1ff1c04fc9dd66d9f000338cdcdba8d79e04
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-17 23:47:49 -0500

    Fixed search bar on header and added hover effects

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,8 +13,8 @@ function Header() {
     navigate(`/products/${input}`);
   };
 
-  const handleSearch = () => {
-    preventDefault();
+  const handleSearch = (e) => {
+    e.preventDefault();
     let enc = encodeURIComponent(searchRef.current.value);
     console.log(enc);
     navigate(`/products/${enc}`);
@@ -22,9 +22,9 @@ function Header() {
 
   return (
     <nav class="flex w-full h-20 justify-between items-center bg-black text-white ">
-      <div class="ml-5 flex justify-between  ">
+      <div class="ml-5 flex justify-between">
         <Link to="/">
-          <img src={logo} alt="Logo" class="w-15" />
+          <img src={logo} alt="Logo" class="w-15 hover:w-20" />
         </Link>
       </div>
       <div class="hidden sm:flex text-lg ml-[13%] md:ml-[10%] lg:ml-[8%] md:text-xl gap-4">
@@ -42,14 +42,14 @@ function Header() {
         </a>
       </div>
 
-      <div class="flex mr-5 gap-2">
-        <form onSubmit={handleSearch}>
-          <input class="w-40 h-7 bg-white text-gray-700 " type="search" placeholder=" Search" ref={searchRef} />
+      <div class="flex mr-5 gap-2.5 justify-end">
+        <form onSubmit={handleSearch} class="w-1/2 md:w-50">
+          <input ref={searchRef} type="text" placeholder="Search" class="border px-3 py-1 rounded-lg w-full" />
         </form>
 
-        <Link to="/cart" class="flex">
+        <Link to="/cart" class="flex gap-1">
+          <FaShoppingCart class="size-6 hover:size-7" />
           <p>{cartItems.length}</p>
-          <FaShoppingCart class="size-6" />
         </Link>
       </div>
     </nav>
