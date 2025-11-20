4ba936b9d2925d6dc1b096a8a7ff8b541c2e1cdd
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-19 23:06:20 -0500

    Fixed type in className

--- online-store-client/src/components/Home/Card.jsx
+++ online-store-client/src/components/Home/Card.jsx
@@ -1,9 +1,9 @@
 function Card(props) {
   return (
-    <div classNameName="relative w-96 h-64 mt-5 mb-5 shrink-0 cursor-point hover:w-100">
-      <img classNameName="inset-0 absolute object-cover w-full h-full" src={props.img} />
-      <div classNameName="absolute left-0 right-0 bottom-0 flex flex-col items-center text-center ">
-        <h3 classNameName="text-black">{props.text}</h3>
+    <div className="relative w-96 h-64 mt-5 mb-5 shrink-0 cursor-point hover:w-100">
+      <img className="inset-0 absolute object-cover w-full h-full" src={props.img} />
+      <div className="absolute left-0 right-0 bottom-0 flex flex-col items-center text-center ">
+        <h3 className="text-black">{props.text}</h3>
       </div>
     </div>
   );
