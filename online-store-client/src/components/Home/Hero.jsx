91098d0454a6f066e7890af0c81f18dc7f99fe4f
Khavar Facey <108436274+khavarpython@users.noreply.github.com>
2025-11-23 13:18:15 -0500

    Hero video fix

--- online-store-client/src/components/Home/Hero.jsx
+++ online-store-client/src/components/Home/Hero.jsx
@@ -6,31 +6,32 @@ function Hero() {
   const handleNavigate = () => {
     navigate("/products");
   };
-
+  
   return (
     <div className="relative w-full h-screen">
       <video
         className="absolute inset-0 w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
+        playsInline
         disablePictureInPicture
-        loop="loop"
-        autoPlay="autoplay"
-        muted>
-        <source src={hero_video} type="video/mp4"></source>
+      >
+        <source src={hero_video} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-
-      <div className="absolute bottom-0 left-0 right-0 pb-12 flex flex-col items-center text-white text-center ">
+      <div className="absolute bottom-0 left-0 right-0 pb-12 flex flex-col items-center text-white text-center">
         <h2 className="text-7xl font-extrabold">Online Store</h2>
         <p className="text-sm">This website was designed by Khavar Facey</p>
         <button
           className="bg-white text-gray-700 px-5 py-1 rounded-xl mt-2 cursor-pointer hover:bg-gray-300"
-          onClick={() => {
-            handleNavigate();
-          }}>
+          onClick={handleNavigate}
+        >
           Shop
         </button>
       </div>
     </div>
   );
 }
+
 export default Hero;
