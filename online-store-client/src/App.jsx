595bebc5930eef42c5f7dcb940c308512d43a6a7
Khavar Facey <khavarfaceyu@gmail.com>
2025-11-22 19:27:12 -0500

    Added success and about page to app routes

--- online-store-client/src/App.jsx
+++ online-store-client/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
+import About from "./pages/About";
+import Success from "./pages/Success";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/products/:postId" element={<Products />} />
         <Route path="/product/:postId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
     </>
   );
